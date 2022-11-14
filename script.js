9131842a6eeb8d920fc7a6c89f511d2a659ac877
Tatiana <Bowmantatiana2@gmail.com>
2022-11-14 13:15:44 -0800

    added script button joke

---
+++ script.js
@@ -0,0 +1,9 @@
+const button = document.getElementById("click");
+
+function clickImg() {
+    alert("STOP TOUCHING MY FACE!");
+    alert("jk lol");
+}
+
+
+button.addEventListener("click", clickImg);
