163c7fb9448bd18c50b4dfdb33934a3fc7e899e6
Tatiana <Bowmantatiana2@gmail.com>
2023-03-30 07:36:02 -0700

    implemented carousel, changed some css, and made some backend changes

--- src/components/CenterCard.js
+++ src/components/CenterCard.js
@@ -1,3 +1,4 @@
+import React from "react"
 import Content from "./Content"
 import ContactInfo from "./ContactInfo"
 
@@ -6,10 +7,18 @@ import ContactInfo from "./ContactInfo"
 
 function CenterCard({url}) {
 
+  const handleFaceClick = () => {
+      alert("no touching!!")
+
+      setTimeout(() => {
+        alert("just kidding!")
+      }, 1000)
+  }
+
     return (
     <div className="center">
         <div className="profpic">
-          <img src="https://i.postimg.cc/76tsQLV5/IMG-3399.jpg" alt="my face"/>
+          <img onClick={handleFaceClick} src="https://i.postimg.cc/76tsQLV5/IMG-3399.jpg" alt="my face"/>
         </div>
         <Content/>
         <ContactInfo/>
